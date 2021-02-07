a82aa2dd408c2baac193adff8c278ba10bd84491
simone <sorzesimone1@gmail.com>
2021-02-07 19:53:25 +0100

    displaying tags inside the gui

--- index.py
+++ index.py
@@ -1,10 +1,10 @@
 import requests
 import validators
-import html.parser
 from bs4 import BeautifulSoup
 from tkinter import *
 
 # ================ GUI Specifications ================ #
+
 root = Tk()
 root.title("Test")
 root.geometry("600x300")
@@ -17,27 +17,34 @@ def start():
     validation = validators.url(url)
 
     if validation == True:
-        print('Works')
         f = requests.get(url)
         soup = BeautifulSoup(f.text, 'html.parser')
         rawtagsList = []
         for tag in soup.find_all():
             rawtagsList.append(tag.name)
+
         cleanList = list(dict.fromkeys(rawtagsList))
-        print(cleanList)
+
+        for uniqueTag in cleanList:
+            tagLabelName = uniqueTag
+            labelTags = Label(root, text=tagLabelName)
+            labelTags.pack()
     else:
         print("Please insert a valid URL")
 
 
 ##################### CREATING WIDGETS #####################
-button_start = Button(root, text="Start", command=start)
-button_quit = Button(root, text="Quit", command=root.quit)
-input_url = Entry(root, width=50)
+
+button_start = Button(root, text="Start", padx=10, pady=5, command=start)
+button_quit = Button(root, text="Quit", padx=10, pady=5, command=root.quit)
+input_url = Entry(root, width=70)
 
 ##################### DISPLAYING WIDGETS #####################
+
 button_start.pack()
 input_url.pack()
 button_quit.pack()
 
 # ================ Runner ================ #
+
 root.mainloop()
