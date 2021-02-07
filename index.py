9707e86c2eb3d1ab3bf31a28dca9a81eb2b2bdae
simone <sorzesimone1@gmail.com>
2021-02-07 13:53:02 +0100

    adding quit button

--- index.py
+++ index.py
@@ -3,6 +3,9 @@ from tkinter import *
 root = Tk()
 
 button_intro = Button(root, text="hello!")
+button_quit = Button(root, text="Quit", command=root.quit)
+
 button_intro.pack()
+button_quit.pack()
 
 root.mainloop()
