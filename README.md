116678e747b835d02b667e343237b9c4ec5f527c
simone <sorzesimone1@gmail.com>
2021-02-07 20:50:16 +0100

    centering the gui

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # gui_py
+
+## Credits
+
+[Center a Tkinter Window on the screen](https://www.youtube.com/watch?v=TdTks2eSx3c)
--- index.py
+++ index.py
@@ -7,7 +7,19 @@ from tkinter import messagebox, Tk, Label, Button, Entry, Frame
 
 root = Tk()
 root.title("Test")
-root.geometry("600x300")
+
+# ================ Centering GUI on launch ================ #
+window_width = 500
+window_height = 500
+
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+x = (screen_width / 2)-(window_width / 2)
+y = (screen_height / 2)-(window_height / 2)
+
+root.geometry(f'{window_width}x{window_height}+{int(x)}+{int(y)}')
+
 
 ##################### FUNCTIONALITIES #####################
 
@@ -20,15 +32,16 @@ def start():
         f = requests.get(url)
         soup = BeautifulSoup(f.text, 'html.parser')
         rawtagsList = []
+
         for tag in soup.find_all():
             rawtagsList.append(tag.name)
 
+        countTotalTags = {i: rawtagsList.count(i) for i in rawtagsList}
+        countBox.insert(0, countTotalTags)
+
         cleanList = list(dict.fromkeys(rawtagsList))
-        i_grid = 5
+
         for uniqueTag in cleanList:
-            # tagLabelName = uniqueTag
-            # labelTags = Label(root, text=uniqueTag)
-            # labelTags.pack()
             boxText.insert(0, uniqueTag + ", ")
 
     else:
@@ -47,6 +60,7 @@ button_quit = Button(root, text="Quit", padx=10, pady=5, command=root.quit)
 input_url = Entry(root, width=70)
 button_credits = Button(root, text="Credits", padx=10, pady=5, command=popup)
 boxText = Entry(root, width=70)
+countBox = Entry(root, width=70)
 
 ##################### DISPLAYING WIDGETS #####################
 
@@ -55,6 +69,7 @@ input_url.pack()
 button_quit.pack()
 button_credits.pack()
 boxText.pack()
+countBox.pack()
 
 # ================ Runner ================ #
 
