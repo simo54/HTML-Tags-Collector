be3f0088e7e31af42d13ab82af98db0742d970a3
simone <sorzesimone1@gmail.com>
2021-02-07 14:15:50 +0100

    adding input widget

--- index.py
+++ index.py
@@ -1,11 +1,24 @@
+import requests
 from tkinter import *
 
 root = Tk()
+root.title("Test")
+root.geometry("600x300")
 
+##################### CREATING WIDGETS #####################
 button_intro = Button(root, text="hello!")
 button_quit = Button(root, text="Quit", command=root.quit)
 
+input_url = Entry(root, width=50)
+
+##################### FUNCTIONALITIES #####################
+
+
+##################### DISPLAYING WIDGETS #####################
 button_intro.pack()
 button_quit.pack()
+input_url.pack()
 
+
+# ================ Runner ================ #
 root.mainloop()
--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-def get_url_data(link):
-#     f = requests.get(link)
-#     return f.text
-
-
-# if __name__ == "__main__":
-#     url = "https://github.com/simo54/gui_py"
-#     data = get_url_data(url)
-#     print(data)
